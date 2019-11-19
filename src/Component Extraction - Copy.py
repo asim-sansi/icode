6d4d6cd97370d529b960cb7ba45be5dc867485d6
SyedHusaini <harishusaini@gmail.com>
2019-11-20 00:29:35 +0500

    border-radius extracted

--- src/Component Extraction - Copy.py
+++ src/Component Extraction - Copy.py
@@ -25,6 +25,16 @@ class HTMLComponent:
         self.tag = "img"
         self.value = ""
         self.img = img
+        # noinspection PyDictCreation
+        self.styles = {}
+        self.styles['left'] = str(x) + "px"
+        self.styles['top'] = str(y) + "px"
+        self.styles['width'] = str(w) + "px"
+        self.styles['height'] = str(h) + "px"
+        self.styles['display'] = "block"
+        self.styles['position'] = "absolute"
+        self.styles['text-align'] = "center"
+        self.styles['border'] = "solid black 2px"
         self.x = x
         self.y = y
         self.h = h
@@ -32,22 +42,23 @@ class HTMLComponent:
         self.cnt = cnt
         self.path = ""
         self.sub = []  # sub elements
-        self.styles = {}
-        self.getDominantColor()
+        self.setDominantColor()
 
     def setImage(self, img):
         self.img = img
+
     def getStyle(self):
-        return self.styles;
+        return self.styles
+
     def setCoordinates(self, x, y):
         self.x = x
         self.y = y
 
     def AddSubElement(self, e):
-        self.sub.append(e);
+        self.sub.append(e)
 
     def getSubElements(self):
-        return self.sub;
+        return self.sub
 
     def getImage(self):
         return self.img
@@ -59,10 +70,12 @@ class HTMLComponent:
         return self.x, self.y, self.w, self.h
 
     def setPath(self, p):
-        self.path = p;
+        self.path = p
+
     def getColors(self):
         return (self.bgcolor[2],self.bgcolor[1],self.bgcolor[0]),(self.color[2],self.color[1],self.color[0]);
-    def getDominantColor(self):
+
+    def setDominantColor(self):
         # You may need to convert the color.
         my_img = np.array(self.img, dtype=np.uint8)
         my_img = cv2.cvtColor(my_img, cv2.COLOR_BGR2RGB)
@@ -72,7 +85,7 @@ class HTMLComponent:
         my_colours = pil_image.getcolors(100000)
         if my_colours:
             self.styles['background-color'] = "rgb" + str(max(my_colours, key=itemgetter(0))[1])
-            self.bgcolor=max(my_colours, key=itemgetter(0))[1];
+            self.bgcolor = max(my_colours, key=itemgetter(0))[1];
             my_colours.remove(max(my_colours, key=itemgetter(0)))
             if my_colours:
                 self.styles['color'] = "rgb" + str(max(my_colours, key=itemgetter(0))[1])
@@ -84,20 +97,27 @@ class HTMLComponent:
 
         return
 
+    def set_shape(self, approx):
+        if len(approx) == 3:
+            # self.styles['shape'] = "triangle"
+            self.styles['border-radius'] = str(len(approx)*0)+"px"
+        elif len(approx) == 4:
+            # self.styles['shape'] = "rectangle"
+            self.styles['border-radius'] = str(len(approx)*0)+"px"
+        else:
+            # self.styles['shape'] = "round"
+            self.styles['border-radius'] = str(len(approx)*5)+"px"
+        return
+
     def Code(self):
         code = "<" +\
                "div" +\
-               " STYLE='" +\
-               " position:absolute;" +\
-               " display:block;" +\
-               " top:" + str(self.y) + "px;" +\
-               " left:" + str(self.x) + "px;" +\
-               " width:" + str(self.w) + "px;" +\
-               " height:" + str(self.h) + "px;" +\
-               " text-align:" + "center;" +\
-               " color:" + str(self.styles['color']) + ";" +\
-               " background-color:" + str(self.styles['background-color']) + "'>TEXT</div>\n"
-               # " SRC='" + self.path + "'>"
+               " STYLE='"
+        for key, value in self.styles.items():
+            code += key + ": " + value + ";"
+
+        code += "'>TEXT</div>\n"
+        # " SRC='" + self.path + "'>"
         return code
 
 
@@ -155,18 +175,14 @@ class TEXT(HTMLComponent):
         # code += self.translateText();
         # code += "</" + self.type + ">";
         # return code
-        code = "<" + \
-               "div" + \
-               " STYLE='" + \
-               " position:absolute;" + \
-               " display:block;" + \
-               " top:" + str(self.y) + "px;" + \
-               " left:" + str(self.x) + "px;" + \
-               " width:" + str(self.w) + "px;" + \
-               " height:" + str(self.h) + "px;" + \
-               " text-align:" + "center;" + \
-               " color:" + str(self.styles['color']) + ";" + \
-               " background-color:" + str(self.styles['background-color']) + "'>"+self.translateText()+"</div>\n"
+
+        code = "<" +\
+               "div" +\
+               " STYLE='"
+        for key, value in self.styles.items():
+            code += key + ": " + value + ";"
+
+        code += "'>"+self.translateText()+"</div>\n"
         # " SRC='" + self.path + "'>"
         return code
 
@@ -263,6 +279,8 @@ class HtmlMapper:
                     element = TEXT(new_img, x, y, h, w, 0)
                 else:
                     element = HTMLComponent(new_img, x, y, h, w, 0)
+
+                element.set_shape(approx)
                 if h1[3] != -1:
                     # if new image is less than 80% of parent image
                     # pshape=parentElement.getImage().shape
@@ -275,7 +293,7 @@ class HtmlMapper:
 
         # cv2.imshow('final', minus_img)
         # cv2.waitKey()
-        self.g(image.copy(),webpage)
+        self.g(image.copy(), webpage)
         return webpage
 
 
