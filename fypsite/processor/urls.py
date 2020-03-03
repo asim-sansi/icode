5cc01ac8c29c8c1c979afb81ba8b29b9b7d42025
Asim Sansi <general_iroh@whitelotus.peace>
2020-03-03 22:30:01 +0500

    Classifier Integrated. Must use *Keras==2.2.5 & *tensorflow==1.13.1 to avoid version conflicts. Guide on installing them both is below open command prompt or powershell Keras --> pip3 install keras==2.2.5 Tensorflow --> pip3 install tensorflow==1.13.1 also don't forget to install ABOVE MENTIONED versions in your pycharm's virtual environment and not the latest ones.

--- fypsite/processor/urls.py
+++ fypsite/processor/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('', views.index, name="index"),
+               path('demo/', views.show_form, name="processform"),
+               path('templates/', views.show_templates, name="templates"),
+               path('aboutus/', views.show_aboutus, name="aboutus"),
+               path('contactus/', views.show_contactus, name="contactus"),
                path('startprocess/', views.initiate, name="initiate"),
                # default view call for this app
                ]
